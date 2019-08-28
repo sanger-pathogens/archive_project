a5987a26e2daa56ffef8be289e01fff4ed73570e
kathryn1995 <km22@sanger.ac.uk>
2019-08-28 15:21:53 +0100

    update to mocking

--- archive_project/get_lanes.py
+++ archive_project/get_lanes.py
@@ -7,12 +7,14 @@ class get_lanes:
 	
 	def pf_data(self):
 
-		data = check_output(["pf", "data", "-t", "study", "-i", self.study])
+		data = check_output(["pf", "data", "-t", "study", "-i", self.study])#.decode('ascii').splitlines()
+		print(data)
 		return data
+
+
 	'''
 	def pf_data(self):
-		data = check_output(["pf", "data", "-t", "study", "-i", self.study])
-		data = data.decode('ascii').splitlines()
+		data = check_output(["pf", "data", "-t", "study", "-i", self.study]).decode('ascii').splitlines()
 		if len(data) == 0:
 			print('Unknown study or no data associated with study')
 			return None 
@@ -20,8 +22,7 @@ class get_lanes:
 			print(data)
 			return data 		
 	'''
-'''
-
+study =
 gl = get_lanes(study)
 data = gl.pf_data()
-'''
+
--- archive_project/tests/test_get_lanes.py
+++ archive_project/tests/test_get_lanes.py
@@ -5,17 +5,27 @@ from archive_project import get_lanes as GL
 
 
 class TestGet_lanes(unittest.TestCase):
-
+	'''
 	@mock.patch('archive_project.get_lanes.check_output')
 	def test_pf_data(self,mock_query: unittest.mock.Mock):
 		lane_class = GL.get_lanes("study")
 		actual = lane_class.pf_data()
 		mock_query.assert_called_once_with(['pf', 'data', '-t', 'study', '-i', 'study'])
-		'''
+		
 		actual = lane_class.check_output.return_value = ['data']
 		self.assertEqual(['data'],actual)
 		'''
-		
+	def test_pf_data(self):
+		with mock.patch('archive_project.get_lanes.check_output', return_value='list') as co:
+			lane_class = GL.get_lanes("study")
+			actual = lane_class.pf_data()
+			co.assert_called_once_with(['pf', 'data', '-t', 'study', '-i', 'study'])
+	'''
+	actual = self.database1.read_studies() #Run the function to read studies with prokaryotes as database 
+		_file.assert_called_once_with(self.prok_path) #Check was run with correct file path
+		expected = ['data1', 'data2', 'data3']
+		self.assertEqual(expected,actual) #check output as expected
+	'''
 '''
 
 GL.get_lanes.pf = Mock()
