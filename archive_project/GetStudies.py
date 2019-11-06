076912c3c13a41b7311713410511d0996e0a2948
kathryn1995 <km22@sanger.ac.uk>
2019-11-06 09:48:23 +0000

    Edit get studies to make sure once try to read from file then try as a list

--- archive_project/GetStudies.py
+++ archive_project/GetStudies.py
@@ -6,11 +6,7 @@ def get_studies(studies):
                 studies_from_file = [line.rstrip('\n') for line in f]
                 return studies_from_file
         except FileNotFoundError:
-            print("This file can't be found. This will be interpretted as a list or Please enter a valid path to a file or a list of study names")
-            return []
-    elif type(studies) is list:
-        print'Studies extracted from list'
-        return studies
+            return studies
     else:
         print(type(studies), "is not a valid input type. Please enter path to a file or a list of study names")
         return []
